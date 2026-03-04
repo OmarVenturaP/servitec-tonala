b46919a290f856dd0e7b876dc44c68f309230693
OmarVentura <reparaciones.tonala@hotmail.com>
2026-03-04 13:12:45 -0600

    Configurando y ajustando la aplicación

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Forzamos a que NO busque el compilador de React
+  experimental: {
+    reactCompiler: false,
+  },
+  // Opcional: Si usas imágenes de Cloudinary, añade esto aquí de una vez
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
